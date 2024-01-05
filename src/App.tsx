3eb7c54d0f981f688db4f7f2e4adeeb301529d0b
Jorge Rangel <georgerangelcode@gmail.com>
2024-01-05 05:57:32 -0500

    VansLife project bootstrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
