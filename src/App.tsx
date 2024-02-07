3a744c191abcc65a4ab775a2ccdea6d13588c9af
Jorge Rangel <georgerangelcode@gmail.com>
2024-02-07 17:07:01 -0500

    feat: Nesting the /host routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Vans } from "./pages/Vans/Vans";
 import { VanDetail } from "./pages/Vans/VanDetail";
@@ -17,9 +16,10 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
