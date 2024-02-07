3261c255443dc2909b85a443e96238968a006214
Jorge Rangel <georgerangelcode@gmail.com>
2024-02-07 16:32:24 -0500

    feat: Navbar with a Layout Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Vans } from "./pages/Vans";
 
 import "./server";
 import { VanDetail } from "./pages/VanDetail";
+import { Layout } from "./components/Layout";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
