109c912b601187a4fdf60fbcda994cd52051bee6
Jorge Rangel <georgerangelcode@gmail.com>
2024-02-07 19:22:03 -0500

    feat: creating HostLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Vans } from "./pages/Vans/Vans";
 import { VanDetail } from "./pages/Vans/VanDetail";
 import { Layout } from "./components/Layout";
-import { Dashboard } from "./pages/Host/Dashboard";
 import { Income } from "./pages/Host/Income";
 import { Reviews } from "./pages/Host/Reviews";
+import { HostLayout } from "./components/HostLayout";
+import { Dashboard } from "./pages/Host/Dashboard";
 import "./server";
 
 const App = () => {
@@ -13,10 +15,13 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />}>
+
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
