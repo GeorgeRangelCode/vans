90bd7ea8406f21771a723d516f4bcb191faefc96
Jorge Rangel <georgerangelcode@gmail.com>
2024-02-15 06:28:09 -0500

    feat: Building out Host Van Detail page

--- src/pages/Host/HostVanDetail.tsx
+++ src/pages/Host/HostVanDetail.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Van } from "../../interfaces/types";
+
 export const HostVanDetail = () => {
-  return <h1>HostVanDetail</h1>;
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = useState<Van>();
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then(res => res.json())
+      .then(data => setCurrentVan(data.vans));
+  }, [id]);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <img src={currentVan.imageUrl} width={150} />
+      <h2>{currentVan.name}</h2>
+      <p>{currentVan.price}</p>
+      <p>{currentVan.type}</p>
+    </div>
+  );
 };
